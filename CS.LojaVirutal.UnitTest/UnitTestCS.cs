6b81dc9d61782b2c7d7ac8d1cc5754b6144ac9ca
Rogério Ferreira da Silva <rogeriojfa@gmail.com>
2015-12-07 12:31:57 -0200

    carga inicial

---
+++ CS.LojaVirutal.UnitTest/UnitTestCS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CS.LojaVirutal.UnitTest
+{
+    [TestClass]
+    public class UnitTestCS
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
